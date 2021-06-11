e2eb3b9612a646f938810c0925ca2075807b97cb
weroks <wero@nerz.cbs.mpg.de>
2021-06-11 11:56:18 +0200

    Adapted learning rates for Francisco

--- for_francisco_updated2.sh
+++ for_francisco_updated2.sh
@@ -2,7 +2,7 @@
 python train.py \
 	--dataset E256_hdf5 --parallel --shuffle  --num_workers 4 --batch_size 96 \
 	--num_G_accumulations 1 --num_D_accumulations 1 \
-	--num_D_steps 1 --G_lr 1e-4 --D_lr 4e-4 --D_B2 0.999 --G_B2 0.999 \
+	--num_D_steps 1 --G_lr 6e-6 --D_lr 1e-5 --D_B2 0.999 --G_B2 0.999 \
 	--G_attn 64 --D_attn 64 \
 	--G_nl inplace_relu --D_nl inplace_relu \
 	--SN_eps 1e-6 --BN_eps 1e-5 --adam_eps 1e-6 \
