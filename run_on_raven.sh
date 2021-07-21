399c0305d9d01db5de6e2eea82dd415f7841cd32
Piero Coronica <piero.coronica@mpcdf.mpg.de>
2021-07-21 11:28:40 +0200

    run_on_raven.sh further cleaning

--- run_on_raven.sh
+++ run_on_raven.sh
@@ -11,7 +11,7 @@
 ### TIME LIMIT: e.g.
 ### 1-00:00:00 -> 1 day (Maximum)
 ### 0-00:20:00 -> 20 minutes
-#SBATCH --time=0-01:00:00
+#SBATCH --time=00-03:00:00
 #SBATCH --signal=USR1@300
 
 ### NODE features:
@@ -38,9 +38,11 @@ echo "${SLURM_NODELIST}"
 
 
 DATA_ROOT="/ptmp/pierocor/datasets"  # This should work but you can use a different one
-WEIGHTS_ROOT="/ptmp/pierocor/hvd_out/resume/weights"  # Replace by a folder where you have writing aaccess
-LOGS_ROOT="/ptmp/pierocor/hvd_out/resume/logs"  # Replace by a folder where you have writing aaccess
-SAMPLE_ROOT="/ptmp/pierocor/hvd_out/resume/samples"  # Replace by a folder where you have writing aaccess
+WEIGHTS_ROOT="/ptmp/pierocor/hvd_out/resume/weights"  # Replace by a folder where you have writing access (HVD folder)
+LOGS_ROOT="/ptmp/pierocor/hvd_out/resume/logs"  # Replace by a folder where you have writing access (HVD folder)
+SAMPLE_ROOT="/ptmp/pierocor/hvd_out/resume/samples"  # Replace by a folder where you have writing access (HVD folder)
+# Replace by the backup folder of your best run
+LOAD_FROM="/ptmp/pierocor/BigGan_out/weights/BigGAN_ecoset_cs500_seed40_Gch96_Dch96_bs176_nDs2_Glr1.0e-04_Dlr4.0e-04_Gnlinplace_relu_Dnlinplace_relu_Ginitortho_Dinitortho_Gattn64_Dattn64_Gshared_hier_ema_hinge"
 
 ### Run the program:
 ### Change wathever you want and have fun!
@@ -53,7 +55,7 @@ srun python train.py \
   --dataset ecoset_cs500 \
   --shuffle  --num_workers 0 --batch_size 176 \
   --num_G_accumulations 1 --num_D_accumulations 1 \
-  --num_D_steps 2 --G_lr 4e-4 --D_lr 1.6e-3 --D_B2 0.999 --G_B2 0.999 \
+  --num_D_steps 2 --G_lr 1e-4 --D_lr 4e-4 --D_B2 0.999 --G_B2 0.999 \
   --G_attn 64 --D_attn 64 \
   --G_nl inplace_relu --D_nl inplace_relu \
   --SN_eps 1e-6 --BN_eps 1e-5 --adam_eps 1e-6 \
@@ -68,7 +70,7 @@ srun python train.py \
   --num_best_copies 5 --num_save_copies 2 \
   --copy_in_mem \
   --seed 0 --resume \
-  --load_from /ptmp/pierocor/BigGan_out/weights/BigGAN_ecoset_cs500_seed40_Gch96_Dch96_bs176_nDs2_Glr1.0e-04_Dlr4.0e-04_Gnlinplace_relu_Dnlinplace_relu_Ginitortho_Dinitortho_Gattn64_Dattn64_Gshared_hier_ema_hinge
+  --load_from ${LOAD_FROM}
 
 ### DO NOT USE PARALLEL!!!
 
